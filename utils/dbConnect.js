0965c1f7296fcb462354eb854c4205f20fd91f8b
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-30 10:39:21 -0400

    create dbConnect util function

---
+++ utils/dbConnect.js
@@ -0,0 +1,23 @@
+// This is a database connection function
+import mongoose from 'mongoose';
+
+// Creating connection object
+const connection = {};
+
+async function dbConnect() {
+  // Check if we have connection to our databse
+  if (connection.isConnected) {
+    return;
+  }
+
+  // Connecting to our database
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+
+  connection.isConnected = db.connections[0].readyState;
+}
+
+export default dbConnect;
