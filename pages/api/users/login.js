81a55be2d45ab4525fe8fcc75954a471ed12dd78
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-10-05 17:35:38 -0400

    update login logic

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,11 +1,10 @@
+import bcrypt from 'bcrypt';
 import dbConnect from 'utils/dbConnect';
 import User from 'models/User';
 
 export default async function handler(req, res) {
   const { method } = req;
   const { username, password } = req.body;
-
-  // console.log(req.body);
 
   await dbConnect();
 
@@ -15,9 +14,18 @@ export default async function handler(req, res) {
   }
 
   try {
-    const user = await User.findByCredentials({ username, password });
-    // const user = await User.findOne({ username });
-    console.log(user);
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new Error('Unable to login');
+    }
+
     res.status(200).json({ success: true, data: user });
   } catch (error) {
     res.status(400).json({ success: false, error: error.message });
