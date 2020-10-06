4f6a346d10fc9943aacf0ef278c5308247b5856b
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-10-06 19:36:15 -0400

    create logoutAll API route

---
+++ pages/api/users/logoutAll.js
@@ -0,0 +1,20 @@
+import withAuth from 'utils/auth';
+
+const handler = async (req, res) => {
+  const { method } = req;
+
+  if (method !== 'POST') {
+    res.status(400).json({ success: false });
+    return;
+  }
+
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+export default withAuth(handler);
