b7eeef79c77f2566d536bdbe780ed2eeb59375cd
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-10-07 11:41:48 -0400

    refactor code to use auth middleware

--- pages/api/notes/index.js
+++ pages/api/notes/index.js
@@ -1,33 +1,32 @@
-import dbConnect from 'utils/dbConnect';
+import withAuth from 'middleware/auth';
 import Note from 'models/Note';
 
-export default async function handler(req, res) {
+const handler = async (req, res) => {
   const { method } = req;
 
-  await dbConnect();
-
   switch (method) {
-    case 'GET':
-      try {
-        const users = await User.find({}).select(['username']);
-        res.status(200).json({ success: true, data: users });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
     case 'POST':
       try {
-        const { username, password } = req.body;
-        const email = normalizeEmail(req.body.email);
+        const note = new Note({ ...req.body, owner: req.user._id });
+        await note.save();
 
-        const user = await User.create({ username, email, password });
-        res.status(201).json({ success: true, data: user });
+        res.status(201).json({ success: true, data: note });
       } catch (error) {
         res.status(400).json({ success: false, error: error.message });
+      }
+      break;
+    case 'GET':
+      try {
+        await req.user.populate('notes').execPopulate();
+        res.status(200).json({ success: true, data: req.user.notes });
+      } catch (error) {
+        res.status(400).json({ success: false });
       }
       break;
     default:
       res.status(400).json({ success: false });
       break;
   }
-}
+};
+
+export default withAuth(handler);
