4f1e7a496f1f11287cd3c67157ef419fa8d676b6
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-10-07 09:38:51 -0400

    create patch and delete api routes withAuth

--- pages/api/users/profile.js
+++ pages/api/users/profile.js
@@ -1,4 +1,5 @@
 import withAuth from 'utils/auth';
+import normalizeEmail from 'validator/lib/normalizeEmail';
 
 const handler = async (req, res) => {
   const { method } = req;
@@ -9,6 +10,34 @@ const handler = async (req, res) => {
         res.status(200).json({ success: true, data: req.user });
       } catch (error) {
         res.status(400).json({ success: false });
+      }
+      break;
+    case 'PATCH':
+      try {
+        const { username, email, password } = req.body;
+        const { user } = req;
+        const updates = {
+          ...(username && { username }),
+          ...(email && { email: normalizeEmail(email) }),
+          ...(password && { password }),
+        };
+
+        Object.keys(updates).forEach((update) => {
+          user[update] = updates[update];
+        });
+
+        await user.save();
+        res.status(200).json({ success: true, data: user });
+      } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
+      }
+      break;
+    case 'DELETE':
+      try {
+        await req.user.remove();
+        res.status(200).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
       }
       break;
     default:
