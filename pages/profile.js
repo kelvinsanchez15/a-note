00df4bf32ba9b6b5c02d63b1f6f5c41ba2097ce3
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-10-15 18:39:39 -0400

    add image input form and layout

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,78 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import useUser from 'src/utils/useUser';
+import {
+  Avatar,
+  IconButton,
+  Button,
+  Badge,
+  Container,
+  Typography,
+  TextField,
+} from '@material-ui/core';
+import { PhotoCamera as PhotoCameraIcon } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  },
+  input: {
+    display: 'none',
+  },
+  uploadLabel: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: theme.spacing(2),
+  },
+  uploadIcon: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+    background: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    '&:hover, &.Mui-focusVisible': {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  },
+}));
 
 export default function ProfilePage() {
+  const classes = useStyles();
   const router = useRouter();
-  const { user, loading, loggedOut, mutate } = useUser();
+  const { user, loading } = useUser();
+
+  const [previewSource, setPreviewSource] = useState('');
+
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => setPreviewSource(reader.result);
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    previewFile(file);
+  };
+
+  const uploadImage = async (base64EncodedImage) => {
+    try {
+      await fetch('/api/users/profile', {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ image: base64EncodedImage }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmitFile = (e) => {
+    e.preventDefault();
+    if (!previewSource) return;
+    uploadImage(previewSource);
+  };
 
   // if logged out, redirect to the homepage
   useEffect(() => {
@@ -20,20 +87,97 @@ export default function ProfilePage() {
       <Head>
         <title>Profile</title>
       </Head>
-      <main>
+      <Container maxWidth="sm">
         {loading ? (
           'loading...'
         ) : (
           <>
-            <h1>Welcome,{user.username}</h1>
-            <p>This is your profile page.</p>
-            <ul>
-              <li>{user.email}</li>
-              <li>{user.createdAt}</li>
-            </ul>
+            <Typography variant="h4" align="center" gutterBottom>
+              Profile
+            </Typography>
+
+            <form onSubmit={handleSubmitFile}>
+              <label htmlFor="icon-button-file" className={classes.uploadLabel}>
+                <input
+                  accept="image/*"
+                  className={classes.input}
+                  id="icon-button-file"
+                  type="file"
+                  onChange={handleFileInputChange}
+                />
+                <Badge
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  badgeContent={
+                    // eslint-disable-next-line react/jsx-wrap-multilines
+                    <IconButton
+                      aria-label="upload picture"
+                      component="span"
+                      className={classes.uploadIcon}
+                    >
+                      <PhotoCameraIcon />
+                    </IconButton>
+                  }
+                >
+                  <Avatar
+                    src={previewSource || user.profileImage}
+                    className={classes.avatar}
+                  />
+                </Badge>
+              </label>
+              <TextField
+                fullWidth
+                id="username"
+                name="username"
+                label="Username"
+                autoComplete="username"
+                color="primary"
+                defaultValue={user.username}
+                // {...getFieldProps('username')}
+                // error={errors.username && Boolean(touched.username)}
+                helperText={' '}
+              />
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                autoComplete="email"
+                color="primary"
+                defaultValue={user.email}
+                // {...getFieldProps('email')}
+                // error={errors.email && Boolean(touched.email)}
+                helperText={' '}
+              />
+              <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                autoComplete="current-password"
+                type="password"
+                color="primary"
+                defaultValue={user.password}
+                // {...getFieldProps('password')}
+                // error={errors.password && Boolean(touched.password)}
+                helperText={' '}
+              />
+              <Button
+                fullWidth
+                type="submit"
+                variant="outlined"
+                color="primary"
+                size="large"
+              >
+                Save changes
+              </Button>
+            </form>
           </>
         )}
-      </main>
+      </Container>
     </>
   );
 }
