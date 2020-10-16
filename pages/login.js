e242f4a903201fb922d9a2fc2a2a4793fefced69
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-10-16 16:43:23 -0400

    revamp login page

--- pages/login.js
+++ pages/login.js
@@ -2,10 +2,51 @@ import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import useUser from 'src/utils/useUser';
+import Link from 'src/components/Link';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { Container, TextField, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container,
+  TextField,
+  FormControl,
+  OutlinedInput,
+  FormHelperText,
+  InputLabel,
+  Typography,
+  Button,
+  Avatar,
+  Grid,
+  Snackbar,
+  InputAdornment,
+  IconButton,
+} from '@material-ui/core';
+import {
+  LockOutlined as LockOutlinedIcon,
+  Visibility,
+  VisibilityOff,
+} from '@material-ui/icons';
 import Alert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(1, 0, 2),
+  },
+}));
 
 const initialValues = {
   username: '',
@@ -18,16 +59,18 @@ const validationSchema = Yup.object({
 });
 
 export default function LoginPage() {
+  const classes = useStyles();
   const router = useRouter();
-  const [errorMsg, setErrorMsg] = useState('');
+  const [error, setError] = useState(false);
   const { user, mutate } = useUser();
+  const [showPassword, setShowPassword] = useState(false);
 
   // redirect to home if user is authenticated
   useEffect(() => {
     if (user) router.push('/');
   }, [router, user]);
 
-  async function onSubmit(values, onSubmitProps) {
+  async function onSubmit(values) {
     const res = await fetch('/api/users/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -39,10 +82,10 @@ export default function LoginPage() {
 
     if (res.status === 200) {
       const userObj = await res.json();
-      onSubmitProps.resetForm();
       mutate(userObj);
+      setError(false);
     } else {
-      setErrorMsg('Incorrect username or password. Try again!');
+      setError(true);
     }
   }
 
@@ -54,59 +97,104 @@ export default function LoginPage() {
 
   const { errors, touched, handleSubmit, getFieldProps } = formik;
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <>
       <Head>
         <title>Sign in</title>
       </Head>
 
-      <Container maxWidth="sm">
-        {errorMsg ? <Alert severity="error">{errorMsg}</Alert> : null}
+      <Container maxWidth="xs" component="main">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
 
-        <Typography component="h2" variant="h3" align="center" gutterBottom>
-          Sign in
-        </Typography>
-
-        <form onSubmit={handleSubmit}>
-          <TextField
-            fullWidth
-            id="username"
-            name="username"
-            label="Username"
-            autoComplete="username"
-            variant="outlined"
-            color="primary"
-            {...getFieldProps('username')}
-            error={errors.username && Boolean(touched.username)}
-            helperText={
-              touched.username && errors.username ? errors.username : ' '
-            }
-          />
-          <TextField
-            fullWidth
-            id="password"
-            name="password"
-            label="Password"
-            autoComplete="current-password"
-            type="password"
-            variant="outlined"
-            color="primary"
-            {...getFieldProps('password')}
-            error={errors.password && Boolean(touched.password)}
-            helperText={
-              touched.password && errors.password ? errors.password : ' '
-            }
-          />
-          <Button
-            fullWidth
-            type="submit"
-            variant="outlined"
-            color="primary"
-            size="large"
-          >
+          <Typography component="h1" variant="h5">
             Sign in
-          </Button>
-        </form>
+          </Typography>
+
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              id="username"
+              name="username"
+              label="Username *"
+              autoComplete="username"
+              variant="outlined"
+              {...getFieldProps('username')}
+              error={errors.username && Boolean(touched.username)}
+              helperText={
+                touched.username && errors.username ? errors.username : ' '
+              }
+            />
+
+            <FormControl
+              variant="outlined"
+              fullWidth
+              {...getFieldProps('password')}
+              error={errors.password && Boolean(touched.password)}
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password *
+              </InputLabel>
+              <OutlinedInput
+                id="password"
+                name="password"
+                autoComplete="current-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  // eslint-disable-next-line react/jsx-wrap-multilines
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password *"
+              />
+              <FormHelperText>
+                {touched.password && errors.password ? errors.password : ' '}
+              </FormHelperText>
+            </FormControl>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="/resetpassword" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  Don&apos;t have an account? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+
+        <Snackbar open={error}>
+          <Alert severity="error">
+            Incorrect username or password. Try again!
+          </Alert>
+        </Snackbar>
       </Container>
     </>
   );
