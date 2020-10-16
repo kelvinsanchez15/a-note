e77667c6f680e97fb38fc686dfdc91339fcb4f3a
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-10-16 18:28:49 -0400

    update theme colors

--- src/components/styles/theme.js
+++ src/components/styles/theme.js
@@ -1,21 +1,18 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { red, pink, teal } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = responsiveFontSizes(
   createMuiTheme({
     palette: {
       primary: {
-        main: '#556cd6',
+        main: pink[500],
       },
       secondary: {
-        main: '#19857b',
+        main: teal.A700,
       },
       error: {
         main: red.A400,
-      },
-      background: {
-        default: '#fff',
       },
     },
   })
