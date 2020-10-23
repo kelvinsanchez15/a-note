20e1a2f65f065cf6d09499bbff7322c39447e261
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-10-23 16:32:44 -0400

    parse fetcher errors and pass isLoggedOut function to exported object

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,3 +1,14 @@
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+
+  if (!res.ok) {
+    const error = new Error('Not authorized!');
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default fetcher;
--- src/utils/useUser.js
+++ src/utils/useUser.js
@@ -6,10 +6,11 @@ export default function useUser() {
   const { data, mutate, error } = useSWR('/api/users/profile', fetcher);
 
   const loading = !data && !error;
-  const isLoggedOut = !data && error?.status === 404;
+  const isLoggedOut = !data && error?.status === 401;
 
   return {
     loading,
+    isLoggedOut,
     error: isLoggedOut ? null : error,
     user: data?.data,
     mutate,
